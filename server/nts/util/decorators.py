14a1e107bf903f960c0798bc5404f66e05e32350
Ragil Prasetya <praser05@gmail.com>
2015-05-11 21:20:50 +0800

    initial config for managed vms

--- server/nts/util/decorators.py
+++ server/nts/util/decorators.py
@@ -1,8 +1,10 @@
 from flask import json
+from functools import wraps
 
 
 def jsonify(func):
   """Convert response into json object."""
+  @wraps(func)
   def inner(*args, **kwargs):
     response = func(args, kwargs)
     return json.jsonify(response)
